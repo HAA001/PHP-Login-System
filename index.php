b72cf9222628271a1c903e88d5a78c611dcf72a4
HAA001 <121903251+HAA001@users.noreply.github.com>
2023-01-12 09:31:42 -0600

    commit

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php echo 'Hello World' ; ?>
+<!DOCTYPE HTML>
+<HTML LANG="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" constent="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="follow">
+
+        <title>Page Title</title>
+
+        <base href="/" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+    </head>
+
+<body>
+<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.15.21/dist/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.15.21/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.15.21/dist/js/uikit-icons.min.js"></script>
+
+
+
+</body>
+</html>
