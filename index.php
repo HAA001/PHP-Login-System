b8f9b3b2b9a996810e02d45586e69e32e84d5d44
HAA001 <121903251+HAA001@users.noreply.github.com>
2023-01-12 10:22:36 -0600

    commit

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-<?php echo 'Hello World' ; ?>
-<!DOCTYPE HTML>
-<HTML LANG="en">
+<!DOCTYPE html>
+<HTML lang="en">
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" constent="IE=edge" />
@@ -14,6 +13,45 @@
     </head>
 
 <body>
+
+<div class="uk-section uk-container">
+    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+
+        <form class="uk-form-stacked">
+
+    <div class="uk-margin">
+        <label class="uk-form-label" for="form-stacked-text">Email</label>
+        <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" type="Email" required='required' placeholder="email@yahoo.com">
+        </div>
+    </div>
+
+
+    <div class="uk-margin">
+        <label class="uk-form-label" for="form-stacked-text">Password</label>
+        <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" type="Password" required='required'  placeholder="Your Password">
+        </div>
+    </div>
+
+    <div class="uk-margin">
+        <button class="uk-button uk-button-default" type="Submit">Login</button>
+    </div>
+
+          </form>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
 <!-- UIkit CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.15.21/dist/css/uikit.min.css" />
 
